69ae33e614aeb8dd8fd3e7f381db4cb619cb45e9
Lyashenko Denis <d.lyashenko@efko.ru>
2025-03-22 16:34:23 +0300

    [FRAMEWORK-20250322] Добавлена контракты формы валидации запроса и ее фабрики

---
+++ src/contracts/http/FormRequestInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace xamned\framework\contracts\http;
+
+interface FormRequestInterface
+{
+    function rules(): array;
+
+    function addRule(array $attributes, array|string $rule): void;
+
+    function validate(): void;
+
+    function addError(string $attribute, string $message): void;
+
+    function getErrors(): array;
+
+    function setSkipEmptyValues(): void;
+
+    function getValues(): array;
+}
