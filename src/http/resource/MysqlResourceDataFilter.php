8cda738ce26d1644702e4e9542fba808c7dbe7b2
m.kudryashov <m.kudryashov@efko.ru>
2025-04-04 11:48:09 +0300

    Переписал логику

--- src/http/resource/MysqlResourceDataFilter.php
+++ src/http/resource/MysqlResourceDataFilter.php
@@ -3,13 +3,13 @@
 namespace xamned\framework\http\resource;
 
 use xamned\framework\contracts\db\DataBaseConnectionInterface;
-use xamned\framework\contracts\db\FileQueryBuilderInterface;
+use xamned\framework\contracts\db\MysqlQueryBuilderInterface;
 
 class MysqlResourceDataFilter extends BaseResourceDataFilter
 {
     public function __construct(
         DataBaseConnectionInterface $dbConnection,
-        FileQueryBuilderInterface $queryBuilder,
+        MysqlQueryBuilderInterface $queryBuilder,
     ) {
         $this->setDbConnection($dbConnection);
         $this->setQueryBuilder($queryBuilder);
