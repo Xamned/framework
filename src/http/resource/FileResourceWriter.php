0825fa595ea83107758f35588c6be09733d12388
m.kudryashov <m.kudryashov@efko.ru>
2025-04-04 12:57:33 +0300

    Переписал логику

--- src/http/resource/FileResourceWriter.php
+++ src/http/resource/FileResourceWriter.php
@@ -3,8 +3,9 @@
 namespace xamned\framework\http\resource;
 
 use xamned\framework\contracts\db\DataBaseConnectionInterface;
+use xamned\framework\contracts\resource\file\FileResourceWriterInterface;
 
-class FileResourceWriter extends BaseResourceWriter
+class FileResourceWriter extends BaseResourceWriter implements FileResourceWriterInterface
 {
     public function __construct(
         DataBaseConnectionInterface $dbConnection,
