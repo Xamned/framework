fa10da2fd2d2878fd2fc184d3bdd8fc5b009910b
LnkoDE <d.lyashenko@efko.ru>
2025-09-22 20:48:30 +0300

    [FRAMEWORK-20250905] Поправлено положение условия

--- src/form/AbstractFormRequest.php
+++ src/form/AbstractFormRequest.php
@@ -63,13 +63,13 @@ abstract class AbstractFormRequest implements FormRequestInterface
 
     public function validateAttribute(array|string $rule, string $attribute): void
     {
-        $validator = $this->validatorFactory->create([$rule]);
-
-        $validator->validate($this->$attribute);
-
         if ($this->skipEmptyValues === true && empty($this->$attribute) === true) {
             return;
         }
+        
+        $validator = $this->validatorFactory->create([$rule]);
+
+        $validator->validate($this->$attribute);
 
         if ($validator->hasErrors() === true) {
             $this->addError($attribute, $validator->getErrorsCasesLine());
